df2c145ff29aabd04861a851e089bcb7f754ab1b
Hannes Juurma <hannes.juurma@khk.ee>
2019-09-23 11:38:44 +0300

    PHP print funktsioon

---
+++ 23.09.2019/Print.php
@@ -0,0 +1,13 @@
+<?php
+// Üherealine kommentaar
+/*Mitmerealine
+  kommentaar*/
+
+
+// Printimine print funkstiooni abil
+print "See on esimene php rida<br>";
+print "See on teine php rida<br>";
+print "See on kolmas php rida<br>";
+
+//Echo abil väljastamine
+echo "See on tekst, mis on kirjutatud echo abil";
