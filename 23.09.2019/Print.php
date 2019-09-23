8bd6c469bf051e66ef668447c00b731dfd78f22d
Hannes Juurma <hannes.juurma@khk.ee>
2019-09-23 12:12:16 +0300

    PHP Ülesanne 1

--- 23.09.2019/Print.php
+++ 23.09.2019/Print.php
@@ -1,3 +1,16 @@
+<html lang="et">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex"/>
+    <meta name="author" content="Hannes Juurma">
+    <meta name="description" content="#">
+    <title>PHP</title>
+    <style>
+    </style>
+</head>
+<body>
 <?php
 // Üherealine kommentaar
 /*Mitmerealine
@@ -11,3 +24,6 @@ print "See on kolmas php rida<br>";
 
 //Echo abil väljastamine
 echo "See on tekst, mis on kirjutatud echo abil";
+?>
+</body>
+</html>
---
+++ 23.09.2019/ex_1.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Ülesanne 1
+ * Koosta php fail, mis tulemusena väljastab info:
+ * Sinu Ees ja Perenimi
+ * Sinu kursuse tähis
+ * Sinu email
+*/
+echo "<h1>Info minu kohta</h1>";
+echo "<p>Hannes Juurma</p>";
+echo "<p>VS 19</p>";
+echo "<p>hannes.juurma@email.ee</p>";
