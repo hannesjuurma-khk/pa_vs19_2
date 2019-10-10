4a8fe79e9644aa8ed41fb480b23cfa77c1a59fb5
Hannes Juurma <hannes.juurma@khk.ee>
2019-10-10 12:20:00 +0300

    09.10.2019 - operators

---
+++ 09.10.2019/CtoF.php
@@ -0,0 +1,6 @@
+<?php
+//Muutujad
+$celsius = 36;
+$farenheit = (9 / 5) * $celsius + 32;
+
+echo "Kui Celsius on: " . $celsius . " kraadi, siis Farenheit on " . $farenheit . " kraadi.";
---
+++ 10.10.2019/Logical operators.php
@@ -0,0 +1,17 @@
+<?php
+//Define variables
+$x = 2;
+$y = 3;
+//Logical operators
+var_dump($x == $y); //Tulemus tuleb false, kuna x ei võrdu y (== on loogiline operaator)
+echo "<hr>";
+var_dump($x != $y); //X ei võrdu y-ga ehk true
+echo "<hr>";
+var_dump($x > $y);
+echo "<hr>";
+var_dump($x < $y);
+echo "<hr>";
+var_dump($x >= $y);
+echo "<hr>";
+var_dump($x <= $y);
+echo "<hr>";
