5f27fcb10182b57e2371506828730385aa23a3e4
Hannes Juurma <hannes.juurma@khk.ee>
2019-10-16 15:14:13 +0300

    Arvamismäng

---
+++ 16.10.2019/arvamismang.php
@@ -0,0 +1,25 @@
+<?php
+
+$number = 25;
+$numberFromUser = $_GET['number'];
+if (strlen($numberFromUser) > 0) {
+    // number is ok
+    if ($numberFromUser == $number) {
+        echo 'Arvasid ära, number on ' . $number . '!';
+    } else {
+        // compute the difference
+        $diff = abs($numberFromUser - $number);
+        if ($diff <= 5) {
+            echo 'Tubli, peaaegu arvasid ära! Aga, ';
+        }
+        // if number is not close
+        if ($numberFromUser > $number) {
+            echo 'Pakutud number on liiga suur';
+        } else {
+            echo 'Pakutud number on liiga väike';
+        }
+    }
+} else {
+    echo 'Tuleb pakkuda number!';
+}
+
