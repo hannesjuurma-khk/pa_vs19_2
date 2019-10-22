514c531e90d1290f86331dccbd5aaabc450032b4
Hannes Juurma <hannes.juurma@khk.ee>
2019-10-22 17:02:02 +0300

    algarvu tuvastamine

---
+++ 22.10.2019/prime_numbers.php
@@ -0,0 +1,23 @@
+<?php
+
+function isPrime($number)
+{
+    if ($number < 2) {
+        return "This number is not valid";
+    } else {
+        $divider = 2;
+        while ($number % $divider != 0) {
+            $divider++;
+        }
+        if ($number == $divider) {
+            return $number . " is a prime number";
+
+        } else {
+            return $number . " is not prime number";
+        }
+    }
+}
+
+//use function
+$result = isPrime(rand(1, 10));
+echo $result;
